3f96f00751c6850b6abb2a994aa0a91acc5e8053
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2023-07-04 22:57:23 +0200

    Projectfiles

--- DFF.dpr
+++ DFF.dpr
@@ -2,13 +2,23 @@ program DFF;
 
 uses
   Vcl.Forms,
-  u_main in 'u_main.pas' {Form1};
+  _GameControl in 'Events\_GameControl.pas',
+  O_Player in 'Objects\O_Player.pas',
+  O_Team in 'Objects\O_Team.pas',
+  DM_GameRoutine in 'DataModel\DM_GameRoutine.pas' {_GameRoutine: TDataModule},
+  u_basemain in 'Base\u_basemain.pas' {_basemain},
+  u_main in 'Forms\u_main.pas' {_main},
+  O_Squad in 'Objects\O_Squad.pas',
+  U_TeamView in 'Views\Team\U_TeamView.pas' {_TeamView};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(T_main, _main);
+  Application.CreateForm(T_basemain, _basemain);
+  Application.CreateForm(T_GameRoutine, _GameRoutine);
+  Application.CreateForm(T_TeamView, _TeamView);
   Application.Run;
 end.
