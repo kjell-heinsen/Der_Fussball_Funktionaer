fe367f9548aeaf88e15a6f49be9d0f654565b32d
kjell-heinsen <60947352+kjell-heinsen@users.noreply.github.com>
2024-07-01 23:53:05 +0200

    DataModel/Team

---
+++ DataModel/DM_Team.pas
@@ -0,0 +1,25 @@
+unit DM_Team;
+
+interface
+
+uses
+  System.SysUtils, System.Classes;
+
+type
+  TDataModule1 = class(TDataModule)
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  DataModule1: TDataModule1;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
